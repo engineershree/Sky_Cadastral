3d720bd2b9b64ee8720e6bfa01724abb6e9334dd
Saksham Bhore <sakshambhore8902@gmail.com>
2026-08-25 13:00:12 +0530

    Fix SPA 404 error on page reload across GitHub Pages, Netlify, Vercel, and Apache servers

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import PlotExplorer from './pages/PlotExplorer';
 import DemoAdmin from './pages/DemoAdmin';
@@ -13,6 +13,8 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/plots" element={<PlotExplorer />} />
           <Route path="/admin" element={<DemoAdmin />} />
+          {/* Catch-all route to prevent blank 404 screens */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
     </Router>
